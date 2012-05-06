c927f96320c8fde7c10f1d1dcedb36bb8b459a3e
Toni Gundogdu <legatvs@gmail.com>
2012-05-06 12:20:50 +0300

    Add quvi_get, quvi_get

---
+++ src/api/get.c
@@ -0,0 +1,112 @@
+/* libquvi
+ * Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/** @file get.c */
+
+#include "config.h"
+
+#include <glib.h>
+
+#include "quvi.h"
+/* -- */
+#include "_quvi_s.h"
+
+static QuviError _get(_quvi_t q, QuviInfo info, ...)
+{
+  QuviError rc = QUVI_OK;
+  gpointer *vp = NULL;
+  gdouble *dp = NULL;
+  gchar **sp = NULL;
+  glong *lp = NULL;
+  gint type = 0;
+  va_list arg;
+
+  va_start(arg, info);
+  type = QUVI_INFO_TYPE_MASK & (gint) info;
+
+  switch (type)
+    {
+    case QUVI_INFO_TYPE_DOUBLE:
+      dp = va_arg(arg, gdouble*);
+      if (dp == NULL)
+        rc = QUVI_ERROR_INVALID_ARG;
+      break;
+    case QUVI_INFO_TYPE_STRING:
+      sp = va_arg(arg, gchar**);
+      if (sp == NULL)
+        rc = QUVI_ERROR_INVALID_ARG;
+      break;
+    case QUVI_INFO_TYPE_LONG:
+      lp = va_arg(arg, glong*);
+      if (lp == NULL)
+        rc = QUVI_ERROR_INVALID_ARG;
+      break;
+    case QUVI_INFO_TYPE_VOID:
+      vp = va_arg(arg, gpointer*);
+      if (vp == NULL)
+        rc = QUVI_ERROR_INVALID_ARG;
+      break;
+    default:
+      rc = QUVI_ERROR_INVALID_ARG;
+      break;
+    }
+  va_end(arg);
+
+  if (rc != QUVI_OK)
+    return (rc);
+
+  switch (info)
+    {
+    case QUVI_INFO_RESPONSE_CODE:
+      *lp = q->status.resp_code;
+      break;
+    case QUVI_INFO_ERROR_CODE:
+      *lp = q->status.rc;
+      break;
+    case QUVI_INFO_CURL_HANDLE:
+      *vp = q->handle.curl;
+      break;
+    default:
+      rc = QUVI_ERROR_INVALID_ARG;
+      break;
+    }
+  return (rc);
+}
+
+/** @brief Return information about the library handle
+@sa @ref getting_started
+@ingroup lib
+*/
+void quvi_get(quvi_t handle, QuviInfo info, ...)
+{
+  _quvi_t q = (_quvi_t) handle;
+  gpointer p = NULL;
+  va_list arg;
+
+  /* If G_DISABLE_CHECKS is defined then the check is not performed. */
+  g_return_if_fail(handle != NULL);
+
+  va_start(arg, info);
+  p = va_arg(arg, gpointer);
+  va_end(arg);
+
+  q->status.rc = _get(handle, info, p);
+}
+
+/* vim: set ts=2 sw=2 tw=72 expandtab: */
---
+++ src/api/set.c
@@ -0,0 +1,87 @@
+/* libquvi
+ * Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/** @file set.c */
+
+#include "config.h"
+
+#include <glib.h>
+
+#include "quvi.h"
+/* -- */
+#include "_quvi_s.h"
+
+static QuviError _set(_quvi_t q, QuviOption o, va_list arg)
+{
+  switch (o)
+    {
+    case QUVI_OPTION_MEDIA_SCRIPT_PROTOCOL_CATEGORY:
+      q->opt.scripts.category = va_arg(arg, glong);
+      break;
+    case QUVI_OPTION_RESOLVE_URL_REDIRECTIONS:
+      q->opt.resolve = (gboolean) va_arg(arg, glong) >0;
+      break;
+    case QUVI_OPTION_VERIFY_MEDIA_STREAM_URL:
+      q->opt.verify = (gboolean) va_arg(arg, glong) >0;
+      break;
+    case QUVI_OPTION_SCRIPT_SORT_ORDER:
+      q->opt.scripts.sort_order = va_arg(arg, glong);
+      q->opt.scripts.update_order = TRUE;
+      break;
+    case QUVI_OPTION_SCRIPT_SORT_BY:
+      q->opt.scripts.sort_by = va_arg(arg, glong);
+      q->opt.scripts.update_order = TRUE;
+      break;
+    case QUVI_OPTION_REQUEST_FORMAT:
+      g_string_assign(q->opt.format, va_arg(arg, gchar*));
+      break;
+
+      /* Callback */
+
+    case QUVI_OPTION_CALLBACK_STATUS:
+      q->cb.status = va_arg(arg, quvi_callback_status);
+      break;
+
+      /* Default */
+
+    default:
+      return (QUVI_ERROR_INVALID_ARG);
+    }
+  return (QUVI_OK);
+}
+
+/** @brief Set library handle option
+@sa @ref getting_started
+@ingroup lib
+*/
+void quvi_set(quvi_t handle, QuviOption option, ...)
+{
+  _quvi_t q = (_quvi_t) handle;
+  va_list arg;
+
+  /* If G_DISABLE_CHECKS is defined then the check is not performed. */
+  g_return_if_fail(handle != NULL);
+
+  va_start(arg, option);
+  q->status.rc = _set(handle, option, arg);
+  va_end(arg);
+}
+
+
+/* vim: set ts=2 sw=2 tw=72 expandtab: */
