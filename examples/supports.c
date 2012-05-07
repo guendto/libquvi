8d4503310f8931e9a0c50701fac7308937919ca2
Toni Gundogdu <legatvs@gmail.com>
2012-05-07 23:45:25 +0300

    examples/supports.c: Add -t

--- examples/supports.c
+++ examples/supports.c
@@ -18,14 +18,21 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 #include <glib.h>
 #include <quvi.h>
 
 static void usage()
 {
-  g_printerr("Usage: supports [options] <URL>\n"
-             "Options:\n"
-             "  -S .. Run offline\n");
+  g_printerr(
+    "Usage: supports [options] <URL>\n"
+    "\nOptions:\n"
+    "  -t<value> .. Support type ('m'edia,'p'laylist,'a'ny)\n"
+    "  -s        .. Run online\n"
+    "\nExample:\n"
+    "  supports -s -tp URL ;# Check (online) against playlist scripts\n"
+    "  supports -ta URL    ;# Check against all types of scripts\n"
+    "\nNote: Checking online resolves shortened URLs, e.g. redirections\n");
   exit(0);
 }
 
@@ -35,8 +42,26 @@ extern void cleanup();
 
 typedef quvi_callback_status qcs;
 
-static QuviSupportsMode m = QUVI_SUPPORTS_MODE_ONLINE;
-static QuviSupportsType t = QUVI_SUPPORTS_TYPE_ANY;
+static QuviSupportsMode m = QUVI_SUPPORTS_MODE_OFFLINE;
+static QuviSupportsType t = 0;
+
+static void update_type(gchar c)
+{
+  switch (c)
+    {
+    case 'p':
+      t |= QUVI_SUPPORTS_TYPE_PLAYLIST;
+      break;
+    case 'm':
+      t |= QUVI_SUPPORTS_TYPE_MEDIA;
+      break;
+    case 'a':
+      t = QUVI_SUPPORTS_TYPE_ANY;
+      break;
+    default:
+      g_printerr("[%s]: ignored `%c': unknown type\n", __func__, c);
+    }
+}
 
 quvi_query_formats_t qqf = NULL;
 quvi_playlist_t qp = NULL;
@@ -54,17 +79,31 @@ int main(int argc, char **argv)
 
   for (; i<argc; ++i)
     {
-      if (argv[i][0] == '-')
+      const gchar *arg = argv[i];
+      if (arg[0] == '-')
         {
-          switch (argv[i][1])
+          switch (arg[1])
             {
-            case 'S':
-              m = QUVI_SUPPORTS_MODE_OFFLINE;
+            case 's':
+              m = QUVI_SUPPORTS_MODE_ONLINE;
+              break;
+            case 't':
+              if (strlen(arg) >1)
+                {
+                  gchar *p = (gchar*) &arg[2];
+                  while (*p != '\0')
+                    {
+                      update_type(*p);
+                      ++p;
+                    }
+                }
+              else
+                t = QUVI_SUPPORTS_TYPE_MEDIA;
               break;
             }
         }
       else
-        url = argv[i];
+        url = (gchar*) arg;
     }
 
   if (url == NULL)
@@ -73,11 +112,15 @@ int main(int argc, char **argv)
       return (2);
     }
 
+  t = (t == 0) ? QUVI_SUPPORTS_TYPE_MEDIA : t;
+
+  g_printerr("[%s] mode=0x%x\n", __func__, (gint) m);
+  g_printerr("[%s] type=0x%x\n", __func__, (gint) t);
+
   q = quvi_new();
   exit_if_error();
 
   quvi_set(q, QUVI_OPTION_CALLBACK_STATUS, (qcs) status);
-
   {
     QuviBoolean r = quvi_supports(q, url, m, t);
     g_print("%s : %s\n", url, (r == QUVI_TRUE) ? "yes":"no");
