763d15ed4c33744a3392096f207cdad12cae6430
Toni Gundogdu <legatvs@gmail.com>
2012-09-05 11:00:26 +0300

    examples/script.c: Rewrite using GLib cmdline parser

--- examples/script.c
+++ examples/script.c
@@ -27,142 +27,273 @@
 
 #include "examples.h"
 
-static void usage()
+struct _opts_s
 {
-  g_print("Usage: script [options]\n"
-          "Type\n"
-          "  -p          .. Dump playlist scripts\n"
-          "  -m          .. Dump media scripts [default]\n"
-          "  -s          .. Dump scan scripts\n"
-          "Printing\n"
-          "  -t<arg> =pd .. Property mask, possible values: 'd','p','n','s'\n"
-         );
-  exit(0);
-}
+  gboolean autoproxy;
+  gchar **property;
+  gboolean verbose;
+  gchar *type;
+};
 
-static QuviScriptType type = QUVI_SCRIPT_TYPE_MEDIA;
-static GString *mask = NULL;
+static struct _opts_s opts;
 
-static void parse_mask(gchar *arg)
+static const GOptionEntry entries[] =
 {
-  gint i=2;
+  {
+    "property", 'p', 0, G_OPTION_ARG_STRING_ARRAY, &opts.property,
+    "Script property to output", "PROPERTY"
+  },
+  {
+    "type", 't', 0, G_OPTION_ARG_STRING, &opts.type,
+    "Script type", "TYPE"
+  },
+  {
+    "autoproxy", 'a', 0, G_OPTION_ARG_NONE, &opts.autoproxy,
+    "Enable the autoproxy feature", NULL
+  },
+  {
+    "verbose", 'v', 0, G_OPTION_ARG_NONE, &opts.verbose,
+    "Verbose libcurl output", NULL
+  },
+  {NULL, 0, 0, 0, NULL, NULL, NULL}
+};
 
-  if (strlen(arg) < 3)
-    return;
+struct _property_lookup_s
+{
+  QuviScriptProperty to;
+  const gchar *from;
+};
 
-  mask = g_string_new(NULL);
+static const struct _property_lookup_s property_conv[] =
+{
+  {QUVI_SCRIPT_PROPERTY_FILEPATH, "filepath"},
+  {QUVI_SCRIPT_PROPERTY_FILENAME, "filename"},
+  {QUVI_SCRIPT_PROPERTY_DOMAINS,  "domains"},
+  {QUVI_SCRIPT_PROPERTY_SHA1,     "sha1"},
+  {0, NULL}
+};
 
-  for (; arg[i]; ++i)
+static void dump_script(QuviScriptType type)
+{
+  QuviScriptProperty qsp;
+  gchar *s;
+  gint i,j;
+
+  for (i=0; opts.property[i] != NULL; ++i)
     {
-      switch (arg[i])
+      const gchar *p = opts.property[i];
+      for (j=0; property_conv[j].from != NULL; ++j)
         {
-        case 'd':
-        case 'p':
-        case 'n':
-        case 's':
-          g_string_append_c(mask, arg[i]);
-          break;
-        default:
-          g_printerr("[%s] Unknown property mask character `%c'\n",
-                     __func__, arg[i]);
+          if (g_strcmp0(p, property_conv[j].from) == 0)
+            {
+              qsp = property_conv[j].to;
+              break;
+            }
         }
+      quvi_script_get(q, type, qsp, &s);
+      if (i == 0)
+        g_print("[%s]\n", __func__);
+      g_print("  %s=%s\n", p, s);
     }
 }
 
-static void parse_args(gint argc, gchar **argv)
+static gchar **property_sv()
 {
-  gint i=1;
-  for (; i<argc; ++i)
-    {
-      if (strlen(argv[i]) <2 || argv[i][0] != '-')
-        continue;
+  gchar **r;
+  gint i,j;
 
-      switch (argv[i][1])
-        {
-          /* Type */
-        case 'p':
-          type = QUVI_SCRIPT_TYPE_PLAYLIST;
-          break;
-        case 'm':
-          type = QUVI_SCRIPT_TYPE_MEDIA;
-          break;
-        case 's':
-          type = QUVI_SCRIPT_TYPE_SCAN;
-          break;
-          /* Property mask */
-        case 't':
-          parse_mask(argv[i]);
-          break;
-        default:
-          usage();
-        }
-    }
+  i=0;
+  while (property_conv[i].from != NULL) ++i;
+  r = g_new(gchar*, i+1);
+
+  i=j=0;
+  while (property_conv[j].from != NULL)
+    r[i++] = g_strdup(property_conv[j++].from);
+  r[i] = NULL;
+
+  return (r);
 }
 
-static void dump_script(quvi_t q)
+static gboolean chk_property_values()
 {
-  GString *o = g_string_new("Properties:\n");
-  gchar *prop = mask->str;
-  gint i=0;
+  gchar **v, *s;
+  gboolean r;
 
-  for (; prop[i]; ++i)
+  v = property_sv();
+  r = examples_chk_val_sv(opts.property, v, &s);
+  if (r == FALSE)
     {
-      QuviScriptProperty qsp;
-      switch (prop[i])
-        {
-        case 'd':
-          qsp = QUVI_SCRIPT_PROPERTY_DOMAINS;
-          g_string_append(o, "  d=");
-          break;
-        case 'p':
-          qsp = QUVI_SCRIPT_PROPERTY_FILEPATH;
-          g_string_append(o, "  p=");
-          break;
-        case 'n':
-          qsp = QUVI_SCRIPT_PROPERTY_FILENAME;
-          g_string_append(o, "  n=");
-          break;
-        case 's':
-          qsp = QUVI_SCRIPT_PROPERTY_SHA1;
-          g_string_append(o, "  s=");
-          break;
-        default:
-          g_warning("Invalid property mask character `%c'\n", prop[i]);
-          continue;
-        }
-      {
-        gchar *s = NULL;
-        quvi_script_get(q, type, qsp, &s);
-        g_string_append_printf(o, "%s\n", s);
-      }
+      g_printerr(
+        "error: invalid value (`%s') for the option `--property'\n", s);
     }
-  g_print("%s", o->str);
-  g_string_free(o, TRUE);
+
+  g_strfreev(v);
+  v = NULL;
+
+  return (r);
 }
+
+struct _type_lookup_s
+{
+  QuviScriptType to;
+  const gchar *from;
+};
+
+static const struct _type_lookup_s type_conv[] =
+{
+  {QUVI_SCRIPT_TYPE_PLAYLIST, "playlist"},
+  {QUVI_SCRIPT_TYPE_MEDIA,    "media"},
+  {QUVI_SCRIPT_TYPE_SCAN,     "scan"},
+  {0, NULL}
+};
+
+static gchar **type_sv()
+{
+  gchar **r;
+  gint i,j;
+
+  i=0;
+  while (type_conv[i].from != NULL) ++i;
+  r = g_new(gchar*, i+1);
+
+  i=j=0;
+  while (type_conv[j].from != NULL)
+    r[i++] = g_strdup(type_conv[j++].from);
+  r[i] = NULL;
+
+  return (r);
+}
+
+static gboolean chk_type_values()
+{
+  gchar **v, *s;
+  gboolean r;
+
+  v = type_sv();
+  r = examples_chk_val_s(opts.type, v, &s);
+  if (r == FALSE)
+    {
+      g_printerr(
+        "error: invalid value (`%s') for the option `--type'\n", s);
+    }
+
+  g_strfreev(v);
+  v = NULL;
+
+  return (r);
+}
+
+static QuviScriptType type_n()
+{
+  gint i;
+  for (i=0; type_conv[i].from != NULL; ++i)
+    {
+      if (g_strcmp0(opts.type, type_conv[i].from) == 0)
+        return (type_conv[i].to);
+    }
+  return (QUVI_SCRIPT_TYPE_MEDIA);
+}
+
+static gint opts_new(gint argc, gchar **argv)
+{
+  GOptionContext *c;
+  GOptionGroup *g;
+  GError *e;
+  gint r;
+
+  c = g_option_context_new(NULL);
+  r = EXIT_SUCCESS;
+  g = NULL;
+  e = NULL;
+
+  g_option_context_set_help_enabled(c, TRUE);
+  g_option_context_add_main_entries(c, entries, NULL);
+
+  if (g_option_context_parse(c, &argc, &argv, &e) == FALSE)
+    {
+      g_printerr("error: %s\n", e->message);
+      g_error_free(e);
+      r = EXIT_FAILURE;
+      e = NULL;
+    }
+
+  g_option_context_free(c);
+  c = NULL;
+
+  /* Set the defaults. */
+
+  if (opts.property == NULL)
+    {
+      gchar *v[] = {"filename", "domains", NULL};
+      opts.property = g_strdupv(v);
+    }
+
+  if (opts.type == NULL)
+    opts.type = g_strdup("media");
+
+  /* Check input. */
+
+  if (chk_property_values() == FALSE)
+    return (EXIT_FAILURE);
+
+  if (chk_type_values() == FALSE)
+    return (EXIT_FAILURE);
+
+  return (r);
+}
+
+static void opts_free()
+{
+  g_strfreev(opts.property);
+  opts.property = NULL;
+
+  g_free(opts.type);
+  opts.type = NULL;
+}
+
+typedef quvi_callback_status qcs;
 
 gint main(gint argc, gchar **argv)
 {
+  QuviScriptType type;
+  gint r;
+
   setlocale(LC_ALL, "");
-  parse_args(argc, argv);
 
-  if (mask == NULL)
-    mask = g_string_new("pd"); /* Default. */
-
-  g_printerr("[%s] mask=%s, type=%d\n", __func__, mask->str, type);
-
+  memset(&opts, 0, sizeof(struct _opts_s));
   g_assert(q == NULL);
+
+  r = opts_new(argc, argv);
+  if (r != EXIT_SUCCESS)
+    return (r);
+
   q = quvi_new();
   examples_exit_if_error();
 
-  while (quvi_script_next(q, type) == QUVI_TRUE)
-    dump_script(q);
+  if (opts.autoproxy == TRUE)
+    examples_enable_autoproxy();
 
-  g_string_free(mask, TRUE);
+  if (opts.verbose == TRUE)
+    examples_enable_verbose();
+
+  type = type_n();
+
+  {
+    gchar *p = g_strjoinv(",", opts.property);;
+    g_printerr("[%s] type=%s (0x%x), property=%s\n",
+               __func__, opts.type, type, p);
+    g_free(p);
+    p = NULL;
+  }
+
+  while (quvi_script_next(q, type) == QUVI_TRUE)
+    dump_script(type);
+
+  opts_free();
   examples_cleanup();
 
   g_assert(q == NULL);
-
-  return (0);
+  return (r);
 }
 
 /* vim: set ts=2 sw=2 tw=72 expandtab: */
