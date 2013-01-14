f8299faac32b067201f2b396939882c632dafaf2
Toni Gundogdu <legatvs@gmail.com>
2013-01-14 14:53:38 +0200

    tests: chk_verbose: Set LIBQUVI_SCRIPTS_VERBOSE
    Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

--- tests/lib/env.c
+++ tests/lib/env.c
@@ -50,6 +50,7 @@ void chk_verbose(quvi_t q)
   if (chk("TEST_VERBOSE", NULL) == TRUE)
     {
       CURL *c = NULL;
+      g_setenv("LIBQUVI_VERBOSE_SCRIPTS", "1", TRUE);
       quvi_get(q, QUVI_INFO_CURL_HANDLE, &c);
       curl_easy_setopt(c, CURLOPT_VERBOSE, 1L);
     }
